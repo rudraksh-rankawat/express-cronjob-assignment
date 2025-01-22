e1f21f50afc199846ffb106985af915f131a3d45
rudrakshrankawat <rudraksh.23bcs10004@ms.sst.scaler.com>
2025-01-22 21:34:00 +0530

    Remove Deprecated Options

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const app = express()
 app.use(express.json())
 
 mongoose
-    .connect(uri, {newUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("connected to mongodb"))
-    .catch((err) => console.error('mongodb connection error:', err));
+    .connect(uri)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+
 
 app.use('/auth', authRoutes)
 
